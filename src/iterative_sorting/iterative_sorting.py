1776372625ec20ed70f33cd1555e1c3e7160eebc
Garrett Weems <gwgraphicdesign@gmail.com>
2019-07-15 19:06:00 -0400

    🚧 selection_sort donzo

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,30 +1,18 @@
-# TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
-    # loop through n-1 elements
+arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+# Try 1
+
+
+def selection_sort(arr):
+    # looper
     for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
-
-        # TO-DO: swap
-
-
-
+        smallest_index = i
+        for range_i in range(i, len(arr)):
+            if arr[smallest_index] > arr[range_i]:
+                smallest_index = range_i
+            # The Switcha-roo
+            arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
 
     return arr
 
 
-# TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
-
-    return arr
-
-
-# STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
-
-    return arr
+print(selection_sort(arr1))
