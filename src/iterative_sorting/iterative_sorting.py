f3c766e2e138b0c52d3a0c535e3ab5784555cb8e
Garrett Weems <gwgraphicdesign@gmail.com>
2019-07-15 20:02:09 -0400

    🐛 Fix selection_sort function

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -15,4 +15,30 @@ def selection_sort(arr):
     return arr
 
 
+def selection_sort(arr):
+    # Loop through array
+    for i in range(len(arr)):
+        # Find the minimum element in remaining unsorted array
+        small_i = i
+        for range_i in range(i, len(arr)):
+            if arr[small_i] > arr[range_i]:
+                small_i = range_i
+
+        # Swap the found minimum element with the first element
+        arr[i], arr[small_i] = arr[small_i], arr[i]
+
+    # return that bitch
+    return arr
+
+
 print(selection_sort(arr1))
+# Inital Array: [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+# Sorted Array: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+# STRETCH: implement the Count Sort function below
+
+
+def count_sort(arr, maximum=-1):
+
+    return arr
